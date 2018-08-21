c0d61f96e1a36298b0f3c724cd861ccfb0cee6d3
Sylvain Blondeau <sylvain@wildcodeschool.fr>
2018-08-21 17:37:01 +0200

    suppression item.php

--- public/index.php
+++ public/index.php
@@ -2,19 +2,34 @@
 <html lang="fr">
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Simple MVC - Home</title>
+    <title>Simple MVC</title>
 
     <link rel="stylesheet" href="/assets/css/styles.css">
     <link rel="icon" href="/assets/images/favicon.png">
 </head>
 
+<?php
+// connection BDD (définir les constantes dans un fichier connec.php qui sera inclus)
+require '../app/connec.php';
+$pdo = new \PDO(DSN, USER, PASS);
+
+// requete SQL 
+$query = "SELECT * FROM item";
+$res = $pdo->query($query);
+$items = $res->fetchAll();
+?>
+
+<!-- affichage des articles récupérés par la requête SQL -->
 <body>
     <section>
-        <h1>Bienvenue sur le Simple MVC</h1>
-        <a href="item.php">Voir les items</a>
+        <h1>Les articles</h1>
+        <ul>
+        <?php foreach ($items as $item) : ?>
+            <li><?= $item['title'] ?></li>
+        <?php endforeach ?>
+        </ul>
     </section>
 </body>
 </html>
